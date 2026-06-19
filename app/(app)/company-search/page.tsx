f96eabde97cf467ddb357518f71d4f9f30ce8262
Federico Gustavo Andre <federico.andre@visma.com>
2026-06-19 10:47:05 -0300

    Force dynamic rendering on company search page to avoid stale cache

--- app/(app)/company-search/page.tsx
+++ app/(app)/company-search/page.tsx
@@ -1,6 +1,8 @@
 import { getCampaigns, getCompanySearchJobs } from "./actions"
 import { CompanySearchClient } from "./company-search-client"
 
+export const dynamic = "force-dynamic"
+
 export default async function CompanySearchPage() {
   const [campaigns, jobs] = await Promise.all([getCampaigns(), getCompanySearchJobs()])
   return <CompanySearchClient campaigns={campaigns} initialJobs={jobs} />
